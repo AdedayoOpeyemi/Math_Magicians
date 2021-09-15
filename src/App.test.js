faece01f3324b8ce141468a76a42794a4baf15f8
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-09-15 23:24:38 +0100

    fixed linter errors

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,5 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const calculatorElement = screen.getByClass('.calc-container');
-  expect(calculatorElementt).toBeInTheDocument();
+  expect(calculatorElement).toBeInTheDocument();
 });
