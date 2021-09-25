06786a9a407551c432c1d6a988990204cee4aead
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-09-25 07:18:30 +0100

    Added snapshot tests for components

---
+++ src/components/test/calculator.test.js
@@ -0,0 +1,10 @@
+// import { render, screen } from '@testing-library/react';
+import renderer from 'react-test-renderer';
+import Calculator from '../Calculator';
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<Calculator />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
