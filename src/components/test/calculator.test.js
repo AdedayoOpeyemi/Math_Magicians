5ea7a3dd51f67406940cf25cd34d0fe01cba5e5f
Opeyemi Oyelesi <oyelesiopeyemi@gmail.com>
2021-09-25 08:31:13 +0100

    snapshots and user interactivity test for calculator component

--- src/components/test/calculator.test.js
+++ src/components/test/calculator.test.js
@@ -1,4 +1,4 @@
-// import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, getByLabelText } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import Calculator from '../Calculator';
 
@@ -8,3 +8,35 @@ it('renders correctly', () => {
     .toJSON();
   expect(tree).toMatchSnapshot();
 });
+
+test('renders the changes when the calcultor button is clicked', () => {
+  render(<Calculator />);
+  fireEvent.click(screen.getByText('1'));
+  fireEvent.click(screen.getByText('5'));
+  const typedElement = screen.getByText('15');
+  expect(typedElement.innerHTML).toBe('15');
+});
+
+test('renders the changes when the calcultor button is clicked', () => {
+  render(<Calculator />);
+  fireEvent.click(screen.getByText('1'));
+  fireEvent.click(screen.getByText('5'));
+  fireEvent.click(screen.getByText('+'));
+  fireEvent.click(screen.getByText('3'));
+  const typedElement = screen.getByText('15 + 3');
+  expect(typedElement.innerHTML).toBe('15 + 3');
+});
+
+test('renders the changes when the calcultor button is clicked', () => {
+  render(<Calculator />);
+  fireEvent.click(screen.getByText('1'));
+  fireEvent.click(screen.getByText('5'));
+  fireEvent.click(screen.getByText('+'));
+  fireEvent.click(screen.getByText('3'));
+  let typedElement = screen.getByText('15 + 3');
+  expect(typedElement.innerHTML).toBe('15 + 3');
+  fireEvent.click(screen.getByText('='));
+  typedElement = screen.getByText('18');
+  expect(typedElement.innerHTML).toBe('18');
+});
+
